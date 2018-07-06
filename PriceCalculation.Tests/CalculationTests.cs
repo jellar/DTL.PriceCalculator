82819c04c39bf3617154a1c8ad3b4726f1539e9b
Rajesh Jella <jella.rajesh@gmail.com>
2018-07-06 22:07:50 +0100

    Butter offer added.

--- PriceCalculation.Tests/CalculationTests.cs
+++ PriceCalculation.Tests/CalculationTests.cs
@@ -7,7 +7,7 @@ namespace PriceCalculation.Tests
     public class CalculationTests
     {
         [TestMethod]
-        public void Calculate_Total_Prices()
+        public void Calculate_total_prices()
         {
             var calculator = new Calculator();
 
@@ -30,7 +30,7 @@ namespace PriceCalculation.Tests
         }
 
         [TestMethod]
-        public void Calculate_Milk_Offer_Total_Price()
+        public void Calculate_total_price_with_milk_offer()
         {
             var calculator = new Calculator();
 
@@ -41,6 +41,44 @@ namespace PriceCalculation.Tests
 
             Assert.AreEqual(totalPrice, 3.45m);
 
+        }
+
+        [TestMethod]
+        public void Calculate_total_price_with_butter_offer()
+        {
+            var calculator = new Calculator();
+
+            var basket = new Basket();
+            basket.Add(new Product() { Name = "butter", Quantity = 2, Price = 0.80m });
+            basket.Add(new Product() { Name = "bread", Quantity = 2, Price = 1.00m });
+
+            var totalPrice = calculator.GetTotal(basket);
+
+            Assert.AreEqual(totalPrice, 3.10m);
+
+        }
+
+        [TestMethod]
+        public void Calculate_total_price_with_both_offers()
+        {
+            var calculator = new Calculator();
+
+            var products = new List<Product>()
+            {
+                new Product() {Name = "butter", Quantity = 2, Price = 0.80m},
+                new Product() {Name = "milk", Quantity = 8, Price = 1.15m},
+                new Product() {Name = "bread", Quantity = 1, Price = 1.00m}
+            };
+
+            var basket = new Basket();
+            foreach (var product in products)
+            {
+                basket.Add(product);
+            }
+
+            var totalPrice = calculator.GetTotal(basket);
+
+            Assert.AreEqual(totalPrice, 9.00m);
         }
     }
 }
--- PriceCalculation/OfferFactory.cs
+++ PriceCalculation/OfferFactory.cs
@@ -1,16 +1,38 @@
-﻿namespace PriceCalculation
+﻿using System.Collections.Generic;
+
+namespace PriceCalculation
 {
     public class OfferFactory : IOfferFactory
     {
+        private readonly Dictionary<string, IOffer> _offers = new Dictionary<string, IOffer>();
         public IOffer GetOffer(Product product)
         {
-            return new Offer()
+            // store offers in dictionary
+            if (_offers.TryGetValue(product.Name, out var offer)) return offer;
+            var productName = product.Name.ToLower().Trim();
+            switch (productName)
             {
-                Product = "milk",
-                QuantityRequired = 4,
-                ProductWithOffer = "milk",
-                OfferAmount = 100
-            };
+                case "milk":
+                    offer = new Offer()
+                    {
+                        Product = "milk",
+                        QuantityRequired = 4,
+                        ProductWithOffer = "milk",
+                        OfferAmount = 100
+                    };
+                    break;
+                case "butter":
+                    offer = new Offer()
+                    {
+                        Product = "butter",
+                        QuantityRequired = 2,
+                        ProductWithOffer = "bread",
+                        OfferAmount = 50
+                    };
+                    break;
+            }
+            _offers.Add(product.Name, offer);
+            return offer;
         }
     }
 }
